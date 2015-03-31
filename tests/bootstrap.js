ffd3c2b47d97f9213c94ac86b27406fb207df8af
Ben Manu <ben@silverstripe.com>
2015-03-31 17:09:40 +1300

    Adding initial gh-pages branch with a test index file.

--- tests/bootstrap.js
+++
@@ -1,8 +0,0 @@
-var Window = require("./lib/window");
-
-global.expect = require("expect.js");
-global.window = new Window();
-global.document = window.document;
-global.navigator = window.navigator;
-
-require('../index');
